1fcc0c744ccb3b0b2abd7ff061c96079c6396188
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-05-11 18:45:53 +0530

    Recipe API - Create custom User model with Unit tests

--- django_projects/recipe-api/app/app/settings.py
+++ django_projects/recipe-api/app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'

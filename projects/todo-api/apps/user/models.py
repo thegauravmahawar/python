1fe1b6d79145823c9314555c8dc76cad45761489
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-11-02 10:00:35 +0530

    Django model

--- projects/todo-api/apps/user/models.py
+++ projects/todo-api/apps/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
+    created_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email', 'password'], name='user_email_password_idx')
+        ]
--- projects/todo-api/apps/user/views.py
+++ projects/todo-api/apps/user/views.py
@@ -1,12 +1,19 @@
 from django.http import JsonResponse
-from . import response
 
 
 def ok(request):
-    return JsonResponse({'status': 'success', 'data': 'ok'})
+    return JsonResponse(
+        {
+            'status:': 'success',
+            'data': 'ok'
+        }
+    )
 
 
 def ping(request):
-    return JsonResponse({'status:': 'success', 'data': 'pong!'})
-
-
+    return JsonResponse(
+        {
+            'status:': 'success',
+            'data': 'pong!'
+        }
+    )

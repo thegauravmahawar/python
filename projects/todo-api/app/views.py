94689d6a7b90eeb3c41633e1eee0ea406023b22c
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-11-12 23:56:19 +0530

    UserViewSet

--- projects/todo-api/app/views.py
+++ projects/todo-api/app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+
+    def create(self, request, **kwargs):
+        print('POST')
+        return JsonResponse(status=200, data={'status': 'success', 'data': 'ok'})
+
+    def update(self, request, pk=None, **kwargs):
+        print('POST')
+
+    def retrieve(self, request, pk=None, **kwargs):
+        print('GET')
+
+    def list(self, request, **kwargs):
+        print('LIST')
+
+    def destroy(self, request, pk=None, **kwargs):
+        print('DELETE')
--- projects/todo-api/config/urls.py
+++ projects/todo-api/config/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from app.views import UserViewSet
+
+router = DefaultRouter()
+
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+print(router.urls)
+urlpatterns += router.urls
